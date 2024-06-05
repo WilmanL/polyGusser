18ff626d3de339fecb09679ffddbda1cd3796efc
WilmanL <wli55@calpoly.edu>
2024-06-05 14:08:42 -0700

    test

--- frontend/src/components/Wordle.js
+++ frontend/src/components/Wordle.js
@@ -1,5 +1,5 @@
 import './Wordle.css';
-import { useState, useEffect} from "react";
+import { useState, useEffect, useCallback } from "react";
 
 // hard set querty keyboard for wordle
 const keyboard = [
@@ -8,45 +8,30 @@ const keyboard = [
     ['z', 'x', 'c', 'v', 'b', 'n', 'm',]
 ];
 
-export default function Wordle({document_today, setGuessWord}) {
-
+export default function Wordle({ document_today, setGuessWord }) {
     const [wordleInputs, setWordleInputs] = useState(Array(6).fill(''));
     const [referenceDoc, setReferenceDoc] = useState([]);
 
-    //document today is being updated
+    // document_today is being updated
     useEffect(() => {
         if (document_today.length > 0) {
             setReferenceDoc(prevReferenceDoc => [...prevReferenceDoc, document_today]);
         }
     }, [document_today]);
 
-    useEffect(() => {
-        if (referenceDoc.length > 0){
-            for(let i = 0; i < referenceDoc.length; i++) {
-                renderBlockChars("Empty", i);
-                renderKeyboard("Emptykey", 0);
-                renderKeyboard("Emptykey", 1);
-                renderKeyboard("Emptykey", 2);
-            }
-        }
-    }, [referenceDoc, renderBlockChars, renderKeyboard]);
-
-
-    const renderKeyboard = (keyClass, keyRow) => {
+    const renderKeyboard = useCallback((keyClass, keyRow) => {
         let keys = [];
-        for(let i = 0; i < keyRow.length; i++) {
+        for (let i = 0; i < keyRow.length; i++) {
             let tempClass = keyClass;
-            if(referenceDoc.length > 0){
-                for(let j = 0; j < referenceDoc.length; j++) {
-                    for(let k = 0; k < referenceDoc[j].length; k++) {
-                        if(referenceDoc[j][k].guessCharacter === keyRow[i]) {
-                            if(referenceDoc[j][k].color === 'green') {
+            if (referenceDoc.length > 0) {
+                for (let j = 0; j < referenceDoc.length; j++) {
+                    for (let k = 0; k < referenceDoc[j].length; k++) {
+                        if (referenceDoc[j][k].guessCharacter === keyRow[i]) {
+                            if (referenceDoc[j][k].color === 'green') {
                                 tempClass = 'Greenkey';
-                            }
-                            else if(referenceDoc[j][k].color === 'yellow') {
+                            } else if (referenceDoc[j][k].color === 'yellow') {
                                 tempClass = 'Yellowkey';
-                            }
-                            else if(referenceDoc[j][k].color === 'grey') {
+                            } else if (referenceDoc[j][k].color === 'grey') {
                                 tempClass = 'Graykey';
                             }
                             break;
@@ -57,68 +42,22 @@ export default function Wordle({document_today, setGuessWord}) {
             keys.push(<div className={tempClass} key={`${i}`}>{keyRow[i]}</div>);
         }
         return keys;
-    };
+    }, [referenceDoc]);
 
-    const handleInputChange = (rowIndex, blockIndex) => (event) => {
-        let newWordleInputs = [...wordleInputs];
-        let word = newWordleInputs[rowIndex].split('');
-
-        if (word.length >= 5) {
-            return; // If it is, prevent further input
-        }
-
-        word[blockIndex] = event.target.value;
-        newWordleInputs[rowIndex] = word.join('');
-        setWordleInputs(newWordleInputs);
-      
-        // Move focus to next input field
-        if (blockIndex < 4) {
-          const nextInput = document.getElementById(`input-${rowIndex}-${blockIndex + 1}`);
-          if(nextInput) nextInput.focus();
-        }
-    };
-
-    const handleKeyDown = (rowIndex, blockIndex) => (event) => {
-        if (event.key === 'Enter') {
-          let word = wordleInputs[rowIndex];
-          if (word.length === 5) {
-            console.log(word);
-            setGuessWord(word);
-          }
-        }
-        else if (event.key === 'Backspace') {
-            let newWordleInputs = [...wordleInputs];
-            let word = newWordleInputs[rowIndex].split('');
-            word[blockIndex] = '';
-            newWordleInputs[rowIndex] = word.join('');
-            setWordleInputs(newWordleInputs);
-        
-            // Move focus to previous input field
-            if (blockIndex > 0) {
-              const previousInput = document.getElementById(`input-${rowIndex}-${blockIndex - 1}`);
-              if (previousInput) previousInput.focus();
-            }
-        }
-    };
-
-
-    const renderBlockChars = (blockClassValue, rowIndex) => {
+    const renderBlockChars = useCallback((blockClassValue, rowIndex) => {
         let row = [];
-        for(let j = 0; j < 5; j++) {
+        for (let j = 0; j < 5; j++) {
             let tempClass = '';
-            if(referenceDoc.length > 0 && referenceDoc[rowIndex]) {
+            if (referenceDoc.length > 0 && referenceDoc[rowIndex]) {
                 const currentChar = referenceDoc[rowIndex][j];
-                if(currentChar) {
-                    if(currentChar.color === 'green') {
+                if (currentChar) {
+                    if (currentChar.color === 'green') {
                         tempClass = 'Green';
-                    }
-                    else if(currentChar.color === 'yellow') {
+                    } else if (currentChar.color === 'yellow') {
                         tempClass = 'Yellow';
-                    }
-                    else if(currentChar.color === 'grey') {
+                    } else if (currentChar.color === 'grey') {
                         tempClass = 'Gray';
-                    }
-                    else{
+                    } else {
                         tempClass = 'Empty';
                     }
                 }
@@ -134,8 +73,7 @@ export default function Wordle({document_today, setGuessWord}) {
                         key={`${j}`} 
                     />
                 );
-            }
-            else{
+            } else {
                 row.push(
                     <input 
                         id={`input-${rowIndex}-${j}`}
@@ -150,32 +88,84 @@ export default function Wordle({document_today, setGuessWord}) {
             }
         }
         return row;
+    }, [referenceDoc, wordleInputs]);
+
+    useEffect(() => {
+        if (referenceDoc.length > 0) {
+            for (let i = 0; i < referenceDoc.length; i++) {
+                renderBlockChars("Empty", i);
+                renderKeyboard("Emptykey", 0);
+                renderKeyboard("Emptykey", 1);
+                renderKeyboard("Emptykey", 2);
+            }
+        }
+    }, [referenceDoc, renderBlockChars, renderKeyboard]);
+
+    const handleInputChange = (rowIndex, blockIndex) => (event) => {
+        let newWordleInputs = [...wordleInputs];
+        let word = newWordleInputs[rowIndex].split('');
+
+        if (word.length >= 5) {
+            return; // If it is, prevent further input
+        }
+
+        word[blockIndex] = event.target.value;
+        newWordleInputs[rowIndex] = word.join('');
+        setWordleInputs(newWordleInputs);
+
+        // Move focus to next input field
+        if (blockIndex < 4) {
+            const nextInput = document.getElementById(`input-${rowIndex}-${blockIndex + 1}`);
+            if (nextInput) nextInput.focus();
+        }
     };
 
-  return (
-    <div className="wordle">
-        <div className="line"></div>
-        <div className="title">
-            <div className="title1">PoLy</div>
-            <div className="title2">GUesSeR</div>
-        </div>
-        <div className="bar"></div>
+    const handleKeyDown = (rowIndex, blockIndex) => (event) => {
+        if (event.key === 'Enter') {
+            let word = wordleInputs[rowIndex];
+            if (word.length === 5) {
+                console.log(word);
+                setGuessWord(word);
+            }
+        } else if (event.key === 'Backspace') {
+            let newWordleInputs = [...wordleInputs];
+            let word = newWordleInputs[rowIndex].split('');
+            word[blockIndex] = '';
+            newWordleInputs[rowIndex] = word.join('');
+            setWordleInputs(newWordleInputs);
 
-        <div className="words">
-            {Array(6).fill().map((_, rowIndex) => (
-            <div className="cellRow" key={rowIndex}>
-                {renderBlockChars("Empty", rowIndex)}
-            </div>
-            ))}
-        </div>
+            // Move focus to previous input field
+            if (blockIndex > 0) {
+                const previousInput = document.getElementById(`input-${rowIndex}-${blockIndex - 1}`);
+                if (previousInput) previousInput.focus();
+            }
+        }
+    };
 
-        <div className="keyboard">
-        {keyboard.map((keyRow, rowIndex) => (
-            <div className="keyRow" key={rowIndex}>
-            {renderKeyboard("Emptykey", keyRow)}
+    return (
+        <div className="wordle">
+            <div className="line"></div>
+            <div className="title">
+                <div className="title1">PoLy</div>
+                <div className="title2">GUesSeR</div>
             </div>
-        ))}
+            <div className="bar"></div>
+
+            <div className="words">
+                {Array(6).fill().map((_, rowIndex) => (
+                    <div className="cellRow" key={rowIndex}>
+                        {renderBlockChars("Empty", rowIndex)}
+                    </div>
+                ))}
+            </div>
+
+            <div className="keyboard">
+                {keyboard.map((keyRow, rowIndex) => (
+                    <div className="keyRow" key={rowIndex}>
+                        {renderKeyboard("Emptykey", keyRow)}
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
